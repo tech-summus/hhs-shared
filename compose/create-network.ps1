f2f0392f9a76d1aa06b53433bc889f12a9331a99
Hasan H. SAHIN <hasansahin@araskargo.com.tr>
2024-05-25 18:44:52 +0300

    ok

--- compose/create-network.ps1
+++
@@ -1,5 +0,0 @@
-docker network create -d bridge hhs.infrastructure-network
-
-docker-compose -f compose/infrastructure.yml -p hhs-infra up -d
-docker-compose -f -p hhs-infra logs --follow
-docker-compose -f compose/infrastructure.yml -p hhs-infra down
