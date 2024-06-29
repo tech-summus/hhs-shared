55d4a844b8a2837adb4089eabc927d64fc839d4e
Hasan H. SAHIN <hasansahin@araskargo.com.tr>
2024-06-29 16:03:33 +0300

    ok

---
+++ src/Hhs.Shared.Hosting.Microservices/Models/FrameworkLogModel.cs
@@ -0,0 +1,20 @@
+using HsnSoft.Base.Logging;
+
+namespace Hhs.Shared.Hosting.Microservices.Models;
+
+public sealed class FrameworkLogModel : IPersistentLog
+{
+    public string LogId { get; set; } = Guid.NewGuid().ToString();
+    public string CorrelationId { get; set; } /*HttpContext CorrelationId*/
+    public string Facility { get; set; }
+    public string Description { get; set; }
+    public string Reference { get; set; }
+    public StackTraceLogDetail StackTrace { get; set; }
+}
+
+public sealed class StackTraceLogDetail
+{
+    public string StackFileName { get; set; }
+    public string StackMethodName { get; set; }
+    public string StackLineNumber { get; set; }
+}
