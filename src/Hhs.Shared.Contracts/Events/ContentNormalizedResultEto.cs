8ddbbdf19bd7d8eade8a02308e46b2626d4e8b9c
Hasan H. SAHIN <hasansahin@araskargo.com.tr>
2024-07-05 03:26:40 +0300

    ok

--- src/Hhs.Shared.Contracts/Events/ContentNormalizedResultEto.cs
+++ src/Hhs.Shared.Contracts/Events/ContentNormalizedResultEto.cs
@@ -3,14 +3,14 @@ using JetBrains.Annotations;
 
 namespace Hhs.Shared.Contracts.Events;
 
-public sealed record ContentNormalizedResultEto(Guid ContentId, bool IsNormalizedSuccess, Guid NormalizedRequestId,DateTime? ReleaseDate, [CanBeNull] string EncodedNormalizedResult) : IIntegrationEventMessage
+public sealed record ContentNormalizedResultEto(Guid ContentId, bool IsNormalizedSuccess, Guid NormalizedRequestId,DateTime? ReleaseTime, [CanBeNull] string EncodedNormalizedResult) : IIntegrationEventMessage
 {
     public Guid ContentId { get; } = ContentId;
     public bool IsNormalizedSuccess { get; } = IsNormalizedSuccess;
 
     public Guid NormalizedRequestId { get; } = NormalizedRequestId;
 
-    public DateTime? ReleaseDate { get; } = ReleaseDate;
+    public DateTime? ReleaseTime { get; } = ReleaseTime;
 
     [CanBeNull]
     public string EncodedNormalizedResult { get; } = EncodedNormalizedResult;
